ff8233f726a5f532c1c12e18d5cd121598a3bcfd
matskira <61023281+matskira@users.noreply.github.com>
2020-07-28 12:38:18 -0300

    Add files via upload

--- httpcomnode/app.js
+++ httpcomnode/app.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(requisicao, resposta){
-    resposta.end('Bem-vindo ao nosso site!');
+    resposta.end('<h1>Bem-vindo ao nosso site!</h1></br><h4>Teste com resposta NodeJS e HTTP</h4>');
 }).listen(8181);
 
 console.log('Meu servidor está rodando!');
